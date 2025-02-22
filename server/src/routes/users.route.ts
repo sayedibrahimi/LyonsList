5f647703011ec6ab140dadeab9086d01468d07c1
Ethan Perry <ethan.perry03@gmail.com>
2025-02-22 09:58:44 -0500

    Added documentation and better structure to users.route

--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -9,7 +9,13 @@ import {
   deleteUser,
 } from "../controllers/users.controller";
 
+// POST /users - Create a new user
+// GET /users - Get all users
 router.route("/").post(createUser).get(getAllUsers);
+
+// GET /users/:id - Get a user by ID
+// PATCH /users/:id - Update a user by ID
+// DELETE /users/:id - Delete a user by ID
 router.route("/:id").get(getUserById).patch(updateUser).delete(deleteUser);
 
 export default router;
