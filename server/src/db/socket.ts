a65e0b70341e47fa9860e2479ab3ad3d74f9739a
Ethan Perry <ethan.perry03@gmail.com>
2025-04-19 10:55:41 -0400

    feat(socket): enhance socket handling and improve message emission logic

--- server/src/db/socket.ts
+++ server/src/db/socket.ts
@@ -8,32 +8,45 @@ const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const io: SocketIOServer = new Server(server, {
   cors: {
-    origin: "*", // production url
+    origin: "*", // TODO! production url
+    // origin: "http://localhost:5000",
     // methods: ["GET", "POST"],
   },
 });
 
-export function getSocketID(userID: string): Socket | undefined {
+export function getSocketID(userID: string): string | undefined {
   const socketID: Socket | undefined = userSocketMap.get(userID);
   if (!socketID) {
     return undefined;
   }
-  return socketID;
+  return socketID.id;
 }
 
 const userSocketMap: Map<string, Socket> = new Map();
 
 io.on("connection", (socket: Socket) => {
-  console.log("New client connected:", socket.id);
+  console.log("\n\nNew client connected:", socket.id);
 
+  // TODO this may be able to be changed to JWT and use requestAuth to parse the UserID from the token
   const userID: string = socket.handshake.query.userID as string;
-  if (userID) {
-    userSocketMap.set(userID, socket);
-    console.log(`User ${userID} connected with socket ID ${socket.id}`);
-  }
+  console.log("User ID from handshake query:", userID);
 
-  // emit that this user is online
+  if (!userID) {
+    console.log("User ID not provided in handshake query");
+    socket.disconnect(true);
+    return;
+  }
+  userSocketMap.set(userID, socket);
+  console.log(`User ${userID} connected with socket ID ${socket.id}\n\n`);
+
+  // emit that this user is online by updating keys?
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("getOnlineUsers", () => {
+    console.log(
+      `Online users requested ${Array.from(userSocketMap.entries()).map(([key, value]) => `${key}: ${value.id}`)}`
+    );
+  });
 
   // Listen for incoming messages from the client
   socket.on("message", (message) => {
